4c4e71a0634db62bc315316a7287c79a49ae8b73
ManuelARs <m.a.r.s.232001@gmail.com>
2022-09-29 19:14:08 -0500

    toma MARTIN

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -1,3 +1,23 @@
+let ganadoC=0, ganadoH=0;
+let person, person2;
+function nombres() {
+    var userInput = new Array();
+    do {
+       userInput = prompt('Nombre Jugador 1: ');
+       if (userInput.length != 3) {
+          alert("Solo pueden ser tres caracteres");
+       }
+    } while (userInput.length != 3);
+    person=userInput;
+    do {
+        userInput = prompt('Nombre Jugador 2: ');
+        if (userInput.length != 3) {
+           alert("Solo pueden ser tres caracteres");
+        }
+     } while (userInput.length != 3);
+     person2=userInput;
+}
+
 class Bootloader extends Phaser.Scene{
     constructor(){
         super({
@@ -12,13 +32,13 @@ class Bootloader extends Phaser.Scene{
     preload() {
         this.load.path = './assets/';
         this.load.image(['huesos', 'calavera', 'tablero', 'drop', 'ganaste']);
-        this.load.image("verde", "verde.jpg");
         this.load.image("fondo", "fondo.png");
         this.load.image("gato", "gato.png");
         this.load.image("reiniciar", "reiniciar.png");
     }
 
     create() {
+        // HUESOS
         this.x = this.add.image(1030, 300, "huesos").setInteractive();
         this.x.setScale(0.20);
         this.input.setDraggable(this.x);
@@ -49,6 +69,7 @@ class Bootloader extends Phaser.Scene{
         this.x5.setDepth(1);
         this.x5.setName("x");
 
+        // CALAVERAS
         this.circle = this.add.image(300, 420, "calavera").setInteractive();
         this.circle.setScale(0.25);
         this.input.setDraggable(this.circle);
@@ -82,11 +103,6 @@ class Bootloader extends Phaser.Scene{
         this.tablero = this.add.image(675, 375, "tablero").setInteractive();
         this.tablero.setScale(0.8);
         this.tablero.setDepth(1);
-        
-
-        this.cuadro = this.add.image(675, 375, "verde").setInteractive();
-        this.cuadro.setScale(0.6);
-        this.cuadro.setDepth(0);
         
         this.fondo = this.add.image(675, 400, "fondo").setInteractive();
         this.fondo.setScale(1.12);
@@ -158,7 +174,18 @@ class Bootloader extends Phaser.Scene{
         this.ganaste = this.add.image(600, 340, 'ganaste').setName("ganaste");
         this.ganaste.setDepth(2);
         this.ganaste.setScale(0.85);
-        this.ganaste.setAlpha(0)
+        this.ganaste.setAlpha(0);
+        
+        this.score = this.add.text(225, 300, ("Calacas: "+ganadoC), { font: '38px Arial Black' });
+        this.score.setTint(0xF38211);
+        this.score.setDepth(1);
+        this.score2 = this.add.text(950, 200, ("Huesos"+': '+ganadoH), { font: '38px Arial Black' });
+        this.score2.setTint(0xF38211);
+        this.score2.setDepth(1);
+        if(person){
+            this.score.text = person+': '+ganadoC;
+            this.score2.text = person2+': '+ganadoH;
+        }
 
         let movesX = []
         let movesO = []
@@ -172,11 +199,18 @@ class Bootloader extends Phaser.Scene{
         [3, 6, 9]]
         let imagenesCal = [this.circle, this.circle2, this.circle3, this.circle4, this.circle5]
         let imagenesHue = [this.x, this.x2, this.x3, this.x4, this.x5]
-        let timedEvent;
+        let timedEvent;  
 
 
         const eventos = Phaser.Input.Events;
         this.input.on(eventos.DRAG_START, (pointer, obj, dragX, dragY) => {
+            if(!person){
+                nombres();
+            }
+            if(person){
+                this.score.text = person+': '+ganadoC;
+                this.score2.text = person2+': '+ganadoH;
+            }
             obj.setScale(0.15);
         });
         
@@ -186,20 +220,26 @@ class Bootloader extends Phaser.Scene{
         });
         
         this.input.on(eventos.DRAG_END, (pointer, obj, dropzone) => {
-            obj.setScale(0.20);
+            if(obj.name=="x"){
+                obj.setScale(0.20);
+            }
+            if(obj.name=="o"){
+                obj.setScale(0.25);
+            }
+
             if ( !dropzone ) {
                 obj.x = obj.input.dragStartX;
                 obj.y = obj.input.dragStartY;
             }
         });
 
-        this.input.on(eventos.DRAG_ENTER, (pointer, obj, dropzone) => {
-            dropzone.setTint(0xabdcd2);
-            //console.log(dropzone.name)
-        });
-        this.input.on(eventos.DRAG_LEAVE, (pointer, obj, dropzone) => {
-            dropzone.clearTint();
-        });
+        // this.input.on(eventos.DRAG_ENTER, (pointer, obj, dropzone) => {
+        //     dropzone.setTint(0xabdcd2);
+        //     //console.log(dropzone.name)
+        // });
+        // this.input.on(eventos.DRAG_LEAVE, (pointer, obj, dropzone) => {
+        //     dropzone.clearTint();
+        // });
 
         let reiniciarJ = () => {
             this.scene.restart();
@@ -221,6 +261,9 @@ class Bootloader extends Phaser.Scene{
                     if (mov.every(verificar)) {
                         console.log("GANASTE CALAVERA")
                         this.ganaste.setAlpha(1)
+                        // this.score.text = "Calacas: "+ scoreCalaca(1);
+                        ganadoC+=1;
+                        this.score.text = person+': '+ganadoC;
                         timedEvent = this.time.delayedCall(4000, reiniciarJ, [], this);
                         //this.scene.restart();
                     }
@@ -240,8 +283,10 @@ class Bootloader extends Phaser.Scene{
                 const verificar2 = (valor) => movesX.includes(valor);
                 for (let mov of posibilities) {
                     if (mov.every(verificar2)) {
-                        console.log("GANASTE HUESOS")
-                        this.ganaste.setAlpha(1)
+                        console.log("GANASTE HUESOS");
+                        this.ganaste.setAlpha(1);
+                        ganadoH+=1;
+                        this.score.text = person+': '+ganadoH;
                         timedEvent = this.time.delayedCall(4000, reiniciarJ, [], this);
                         //this.scene.restart();
                     }
@@ -255,7 +300,7 @@ class Bootloader extends Phaser.Scene{
             }
 
         });
-
+        
         this.reiniciar.on(eventos.POINTER_OVER, function() {
             this.setScale(0.22);
         });
@@ -266,6 +311,35 @@ class Bootloader extends Phaser.Scene{
             this.scene.restart();
         }, this);
 
+        // if(!person){
+        //     nombres();
+        // }
+
+
+        if(ganadoC>ganadoH){
+            alert("    LeaderBoard \n" + 
+            "Ganando "+person+' Score: '+ganadoC+"\n"+
+            "Perdiendo "+person2+' Score: '+ganadoH);
+            console.log(person+': '+ganadoC)
+            console.log(person2+': '+ganadoH)
+        }
+        if(ganadoH>ganadoC){
+            alert("    LeaderBoard \n" + 
+            "Ganando "+person2+' Score: '+ganadoH+"\n"+
+            "Perdiendo "+person+' Score: '+ganadoC);
+            console.log("LeaderBoard")
+            console.log(person2+': '+ganadoH)
+            console.log(person+': '+ganadoC)
+        }
+        if(ganadoH==ganadoC&&ganadoC>0&&ganadoH==0){
+            alert("    LeaderBoard \n" + 
+            "Empate "+person2+' Score: '+ganadoH+"\n"+
+            "Empate "+person+' Score: '+ganadoC);
+            console.log("LeaderBoard")
+            console.log(person2+': '+ganadoH)
+            console.log(person+': '+ganadoC)
+        }
+        
     }
 
     update(time, delta) {
